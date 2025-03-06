184e9a7a10f0f15027a44d2d2f216506b3dc3991
Moin Uss <moin@usoftsolutions.com>
2025-03-06 17:34:36 +0530

    message

--- src/components/header/sub-components/LanguageCurrencyChanger.js
+++ src/components/header/sub-components/LanguageCurrencyChanger.js
@@ -20,24 +20,12 @@ const LanguageCurrencyChanger = ({
 
   return (
     <div className="language-currency-wrap">
-      <div className="same-language-currency language-style">
+      <div className="same-language-currency ">
         <span>
-          {currentLanguageCode === "en"
-            ? "English"
-            : currentLanguageCode === "fn"
-            ? "French"
-            : currentLanguageCode === "de"
-            ? "Germany"
-            : ""}{" "}
-          <i className="fa fa-angle-down" />
+          Email : info@smartwave.com
         </span>
         <div className="lang-car-dropdown">
           <ul>
-            <li>
-              <button value="en" onClick={e => changeLanguageTrigger(e)}>
-                English
-              </button>
-            </li>
             {/* <li>
               <button value="fn" onClick={e => changeLanguageTrigger(e)}>
                 French
@@ -51,35 +39,7 @@ const LanguageCurrencyChanger = ({
           </ul>
         </div>
       </div>
-      <div className="same-language-currency use-style">
-        <span>
-          {currency.currencyName} <i className="fa fa-angle-down" />
-        </span>
-        <div className="lang-car-dropdown">
-          <ul>
-          <li>
-              <button value="INR" onClick={e => setCurrencyTrigger(e)}>
-                INR
-              </button>
-            </li>
-            {/* <li>
-              <button value="USD" onClick={e => setCurrencyTrigger(e)}>
-                USD
-              </button>
-            </li>
-            <li>
-              <button value="EUR" onClick={e => setCurrencyTrigger(e)}>
-                EUR
-              </button>
-            </li>
-            <li>
-              <button value="GBP" onClick={e => setCurrencyTrigger(e)}>
-                GBP
-              </button>
-            </li> */}
-          </ul>
-        </div>
-      </div>
+
       <div className="same-language-currency">
         <p>Call Us +65 90288134</p>
       </div>
