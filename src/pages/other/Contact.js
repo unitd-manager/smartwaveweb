a9ac0d9fd428d9e52619e323c9c6e109a088ad32
mafmeera <meera@unitdtechnologies.com>
2025-04-10 15:31:43 +0530

    first commit

--- src/pages/other/Contact.js
+++ src/pages/other/Contact.js
@@ -11,10 +11,12 @@ import { useToasts } from "react-toast-notifications";
 export default function Contact({ location }) {
   const { pathname } = location;
   const { addToast } = useToasts();
-  React.useEffect(() => {
+  useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   const applyChanges = () => {};
+
   const [contact, setContact] = useState();
   const [contacts, setContacts] = useState();
   const [email, setEmail] = useState();
@@ -23,11 +25,139 @@ export default function Contact({ location }) {
   const [mailId, setmailId] = useState("");
   const [googlemapdata, setGoogleMapData] = useState('');
 
+  const [user, setUser] = useState({
+    first_name: "",
+    last_name: "",
+    email: "",
+    comments: "",
+    enquiry_code: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+
+  const isValidEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  };
+
+  const validateForm = () => {
+    if (!user.email || !isValidEmail(user.email)) {
+      addToast("Please enter a valid email address.", {
+        appearance: "error",
+        autoDismiss: true,
+      });
+      return false;
+    }
+
+    if (!user.comments || user.comments.trim() === "") {
+      addToast("Please enter your message.", {
+        appearance: "error",
+        autoDismiss: true,
+      });
+      return false;
+    }
+
+    return true;
+  };
+
+  const ContactSubmit = (code) => {
+    if (!validateForm()) return;
+
+    api
+      .post("/commonApi/addEnquiry", {
+        first_name: user.first_name,
+        last_name: user.last_name,
+        email: user.email,
+        comments: user.comments,
+        enquiry_code: code,
+      })
+      .then((res) => {
+        addToast("Thank you for your enquiry submission!", {
+          appearance: "success",
+          autoDismiss: true,
+        });
+        setUser({
+          first_name: "",
+          last_name: "",
+          email: "",
+          comments: "",
+          enquiry_code: "",
+        });
+      })
+      .catch((err) => {
+        addToast("Enquiry submission failed!", {
+          appearance: "error",
+          autoDismiss: true,
+        });
+      });
+  };
+
+  const generateCode = () => {
+    if (!validateForm()) return;
+
+    api
+      .post('/commonApi/getCodeValues', { type: 'enquiry' })
+      .then((res) => {
+        ContactSubmit(res.data.data);
+      })
+      .catch(() => {
+        ContactSubmit('');
+      });
+  };
+
+  const sendMail = () => {
+    if (!validateForm()) return;
+
+    if (window.confirm("Are you sure you want to send Email?")) {
+      const to = mailId.email;
+      const text = user.comments;
+      const subject = user.email;
+      const dynamic_template_data = {
+        first_name: user.first_name,
+        email: user.email,
+        comments: user.comments,
+      };
+
+      api
+        .post("/commonApi/sendemail", {
+          to,
+          text,
+          subject,
+          dynamic_template_data,
+        })
+        .then(() => {
+          addToast("Email has been sent successfully!", {
+            appearance: "success",
+            autoDismiss: true,
+          });
+          setUser({
+            first_name: "",
+            last_name: "",
+            email: "",
+            comments: "",
+            enquiry_code: "",
+          });
+        })
+        .catch((err) => {
+          addToast("Unable to send Email", {
+            appearance: "error",
+            autoDismiss: true,
+          });
+        });
+    } else {
+      applyChanges();
+    }
+  };
+
   const getEnquiryEmail = () => {
     api.get("/setting/getEnquiryMailId").then((res) => {
       setmailId(res.data.data[0]);
     });
   };
+
   const getContact = () => {
     api.get("/contact/getContacts").then((res) => {
       setContact(res.data.data[0]);
@@ -58,119 +188,12 @@ export default function Contact({ location }) {
     });
   };
 
-  const getGoogleMap = () =>{
-    api.get('/setting/getSettingsForGoogleMap').then(res=>{
-      setGoogleMapData(res.data.data[0])
-     })
-  }
-
-  const [user, setUser] = React.useState({
-    first_name: "",
-    last_name: "",
-    email: "",
-    comments: "",
-    enquiry_code: "",
-  });
-
-  let name, value;
-
-  const handleChange = (e) => {
-    name = e.target.name;
-    value = e.target.value;
-    setUser({ ...user, [name]: value });
-    console.log({ [name]: value });
-    // lname = e.target.lname
-    // email = e.target.email
-    // message = e.target.message
-  };
-  const ContactSubmit = (code) => {
-    api
-      .post("/commonApi/addEnquiry", {
-        first_name: user.first_name,
-        last_name: user.last_name,
-        email: user.email,
-        comments: user.comments,
-        enquiry_code: code,
-      })
-      .then((res) => {
-        console.log(res);
-        addToast("Thank you for your enquiry submission!", {
-          appearance: "success",
-          autoDismiss: true,
-        });
-  
-        // Reset form after successful submission
-        setUser({
-          first_name: "",
-          last_name: "",
-          email: "",
-          comments: "",
-          enquiry_code: "",
-        });
-      })
-      .catch((err) => {
-        addToast("Enquiry submission failed!", {
-          appearance: "error",
-          autoDismiss: true,
-        });
-      });
+  const getGoogleMap = () => {
+    api.get('/setting/getSettingsForGoogleMap').then(res => {
+      setGoogleMapData(res.data.data[0]);
+    });
   };
 
-
-  const generateCode = () => {
-    api
-      .post('/commonApi/getCodeValues', { type: 'enquiry' })
-      .then((res) => {
-        ContactSubmit(res.data.data);
-      })
-      .catch(() => {
-        ContactSubmit('');
-      });
-  };
-  
-  const sendMail = () => {
-    if (window.confirm("Are you sure you want to send Email?")) {
-      const to = mailId.email;
-      const text = user.comments;
-      const subject = user.email;
-      const dynamic_template_data = {
-        first_name: user.first_name,
-        email: user.email,
-        comments: user.comments,
-      };
-  
-      api
-        .post("/commonApi/sendemail", {
-          to,
-          text,
-          subject,
-          dynamic_template_data,
-        })
-        .then(() => {
-          addToast("Email has been sent successfully!", {
-            appearance: "success",
-            autoDismiss: true,
-          });
-  
-          // Reset form after successful email
-          setUser({
-            first_name: "",
-            last_name: "",
-            email: "",
-            comments: "",
-          });
-        })
-        .catch((err) => {
-          addToast("Unable to send Email", {
-            appearance: "error",
-            autoDismiss: true,
-          });
-        });
-    } else {
-      applyChanges();
-    }
-  };
-  
   useEffect(() => {
     getMobile();
     getContact();
@@ -180,62 +203,41 @@ export default function Contact({ location }) {
     getEnquiryEmail();
     getGoogleMap();
   }, []);
+
   return (
     <Fragment>
       <MetaTags>
         <title>Smartwave | Contact</title>
-        <meta
-          name="description"
-          content="Contact of UnitdEcom react minimalist eCommerce template."
-        />
+        <meta name="description" content="Contact page for Smartwave" />
       </MetaTags>
       <BreadcrumbsItem to={process.env.PUBLIC_URL + "/"}>Home</BreadcrumbsItem>
-      <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>
-        Contact
-      </BreadcrumbsItem>
+      <BreadcrumbsItem to={process.env.PUBLIC_URL + pathname}>Contact</BreadcrumbsItem>
       <LayoutOne headerTop="visible">
-        {/* breadcrumb */}
         <Breadcrumb />
         <div className="contact-area pt-100 pb-100">
           <div className="container">
             <div className="contact-map mb-10">
-            <LocationMap description={googlemapdata&&googlemapdata.description} />
+              <LocationMap description={googlemapdata && googlemapdata.description} />
             </div>
             <div className="custom-row-2">
               <div className="col-lg-4 col-md-5">
                 <div className="contact-info-wrap">
                   <div className="single-contact-info">
-                    <div className="contact-icon">
-                      <i className="fa fa-phone" />
-                    </div>
+                    <div className="contact-icon"><i className="fa fa-phone" /></div>
                     <div className="contact-info-dec">
-                      <p>
-                        {" "}
-                        <span>{contact && contact.mobile}</span>
-                      </p>
-                      <p>
-                        {" "}
-                        <span>{contacts && contacts.mobile1}</span>
-                      </p>
+                      <p><span>{contact && contact.mobile}</span></p>
+                      <p><span>{contacts && contacts.mobile1}</span></p>
                     </div>
                   </div>
                   <div className="single-contact-info">
-                    <div className="contact-icon">
-                      <i className="fa fa-globe" />
-                    </div>
+                    <div className="contact-icon"><i className="fa fa-globe" /></div>
                     <div className="contact-info-dec">
-                      <p>
-                        <span>{email && email.mailId}</span>
-                      </p>
-                      <p>
-                        <span>{website && website.web}</span>
-                      </p>
+                      <p><span>{email && email.mailId}</span></p>
+                      <p><span>{website && website.web}</span></p>
                     </div>
                   </div>
                   <div className="single-contact-info">
-                    <div className="contact-icon">
-                      <i className="fa fa-map-marker" />
-                    </div>
+                    <div className="contact-icon"><i className="fa fa-map-marker" /></div>
                     <div className="contact-info-dec">
                       <span>{address && address.addr}</span>
                     </div>
@@ -243,31 +245,11 @@ export default function Contact({ location }) {
                   <div className="contact-social text-center">
                     <h3>Follow Us</h3>
                     <ul>
-                      <li>
-                        <a href="//facebook.com">
-                          <i className="fa fa-facebook" />
-                        </a>
-                      </li>
-                      <li>
-                        <a href="//pinterest.com">
-                          <i className="fa fa-pinterest-p" />
-                        </a>
-                      </li>
-                      <li>
-                        <a href="//thumblr.com">
-                          <i className="fa fa-tumblr" />
-                        </a>
-                      </li>
-                      <li>
-                        <a href="//vimeo.com">
-                          <i className="fa fa-vimeo" />
-                        </a>
-                      </li>
-                      <li>
-                        <a href="//twitter.com">
-                          <i className="fa fa-twitter" />
-                        </a>
-                      </li>
+                      <li><a href="//facebook.com"><i className="fa fa-facebook" /></a></li>
+                      <li><a href="//pinterest.com"><i className="fa fa-pinterest-p" /></a></li>
+                      <li><a href="//thumblr.com"><i className="fa fa-tumblr" /></a></li>
+                      <li><a href="//vimeo.com"><i className="fa fa-vimeo" /></a></li>
+                      <li><a href="//twitter.com"><i className="fa fa-twitter" /></a></li>
                     </ul>
                   </div>
                 </div>
@@ -279,54 +261,53 @@ export default function Contact({ location }) {
                   </div>
                   <form className="contact-form-style">
                     <div className="row">
-                      <div class="col-lg-6">
+                      <div className="col-lg-6">
                         <input
                           type="text"
-                          class="form-control mb-4"
+                          className="form-control mb-4"
                           placeholder="First Name"
                           name="first_name"
                           value={user.first_name}
-                          onChange={(event) => handleChange(event)}
+                          onChange={handleChange}
                         />
                       </div>
-                      <div class="col-lg-6">
+                      <div className="col-lg-6">
                         <input
                           type="text"
-                          class="form-control mb-4"
+                          className="form-control mb-4"
                           placeholder="Last Name"
                           name="last_name"
                           value={user.last_name}
-                          onChange={(event) => handleChange(event)}
+                          onChange={handleChange}
                         />
                       </div>
-                      <div class="col-12">
+                      <div className="col-12">
                         <input
                           type="text"
-                          class="form-control mb-4"
+                          className="form-control mb-4"
                           placeholder="Email"
                           name="email"
                           value={user.email}
-                          onChange={(event) => handleChange(event)}
+                          onChange={handleChange}
                         />
                       </div>
-                      <div class="col-12">
+                      <div className="col-12">
                         <textarea
                           name="comments"
-                          class="form-control mb-4"
+                          className="form-control mb-4"
                           placeholder="Message"
                           value={user.comments}
-                          onChange={(event) => handleChange(event)}
+                          onChange={handleChange}
                         ></textarea>
                       </div>
-                      <div class="col-8">
+                      <div className="col-8">
                         <button
-                          color="success"
+                          type="button"
+                          className="btn btn-primary"
                           onClick={() => {
                             sendMail();
                             generateCode();
                           }}
-                          type="button"
-                          className="btn btn-primary"
                         >
                           Submit Now
                         </button>
