348c1abc67a43d9a45c37e11d5554befea4bdeed
Muthumari <muthumari@unitdtechnologies.com>
2025-12-10 10:59:52 +0530

    featuresession

--- src/components/FeaturesSection.js
+++ src/components/FeaturesSection.js
@@ -2,12 +2,19 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import "./FeaturesSection.css";
 
+// const features = [
+//   { id: 1, title: "Eat Healthier", text: "Modi tempora incidunt ut labore dolore magnam aliquam", icon: "🍏" },
+//   { id: 2, title: "Fresh And Clean Products", text: "Modi tempora incidunt ut labore dolore magnam aliquam", icon: "🫙" },
+//   { id: 3, title: "We Have Brands", text: "Modi tempora incidunt ut labore dolore magnam aliquam", icon: "💰" },
+//   { id: 4, title: "Modern Process", text: "Modi tempora incidunt ut labore dolore magnam aliquam", icon: "🚜" },
+// ];
+
 const features = [
-  { id: 1, title: "Eat Healthier", text: "Modi tempora incidunt ut labore dolore magnam aliquam", icon: "🍏" },
-  { id: 2, title: "Fresh And Clean Products", text: "Modi tempora incidunt ut labore dolore magnam aliquam", icon: "🫙" },
-  { id: 3, title: "We Have Brands", text: "Modi tempora incidunt ut labore dolore magnam aliquam", icon: "💰" },
-  { id: 4, title: "Modern Process", text: "Modi tempora incidunt ut labore dolore magnam aliquam", icon: "🚜" },
+  { id: 1, title: "10+", text: "Customers" },
+  { id: 2, title: "50+", text: "Containers" },
+  { id: 3, title: "4", text: "Countries" },
 ];
+
 
 const FeaturesSection = () => {
   return (
@@ -16,8 +23,7 @@ const FeaturesSection = () => {
         {features.map((feature) => (
           <Col key={feature.id} md={6} lg={3} className="feature-item">
             <div className="feature-content">
-              <div className="feature-icon">{feature.icon}</div>
-              <h5 className="feature-title">{feature.title}</h5>
+              <h1 className="feature-title">{feature.title}</h1>
               <p className="feature-text">{feature.text}</p>
             </div>
           </Col>
